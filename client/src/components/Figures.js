812bd4d9f43535c93e76fde7271188732dd44a54
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2021-07-28 10:21:28 -0700

    add infinit scroll

--- client/src/components/Figures.js
+++ client/src/components/Figures.js
@@ -1,41 +1,66 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 import "./Figures.css";
 
 const Figures = () => {
   const [data, setData] = useState([]);
+  const [dataOffset, setDataOffset] = useState("");
+  const requestAmount = 20;
+
+  const requestData = () => {
+    axios
+      .get("/api/v1/covic-data/figures", {
+        params: { offset: dataOffset, requestAmount },
+      })
+      .then(response => {
+        setData(data.concat(response.data.records));
+        setDataOffset(response.data.offset);
+      });
+  };
   useEffect(() => {
-    axios.get("/api/v1/covic-data/figures").then(response => {
-      console.log(response.data);
-      setData(response.data);
-    });
+    requestData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleScroll = () => {
+    requestData();
+  };
+
   return (
-    <div className='box'>
-      {data.map(item => {
-        if (
-          item.fields?.Image[0] &&
-          item.fields?.Image[0]?.thumbnails?.large.url !== undefined
-        ) {
-          return (
-            <div className='card' key={item.id}>
-              <img
-                src={item.fields?.Image[0]?.thumbnails?.large.url}
-                alt=''
-                className='card--image'
-              />
-              <div className='card--label'>
-                {item.fields["Date (from Article)"]}
+    <>
+      <InfiniteScroll
+        dataLength={data.length}
+        next={handleScroll}
+        hasMore={true}
+        className='box'
+        loader={<CircularProgress className='loader' />}
+      >
+        {data.map(item => {
+          if (
+            item.fields?.Image[0] &&
+            item.fields?.Image[0]?.thumbnails?.large.url !== undefined
+          ) {
+            return (
+              <div className='card' key={item.id}>
+                <img
+                  src={item.fields?.Image[0]?.thumbnails?.large.url}
+                  alt=''
+                  className='card--image'
+                />
+                <div className='card--label'>
+                  {item.fields["Date (from Article)"]}
+                </div>
               </div>
-            </div>
-          );
-        } else {
-          return null;
-        }
-      })}
-    </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </InfiniteScroll>
+    </>
   );
 };
 
