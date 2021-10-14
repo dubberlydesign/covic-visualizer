9bf35547750e59bc8cf1d00e41f940f5f6a067b1
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2021-10-13 17:43:50 -0700

    CV-39 article rework

---
+++ client/src/components/GridContent.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import classNames from 'classnames';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const GridContent = ({item, classes, getDisplayLabels, handleOpen}) => {
+  return (
+    <>
+      <Typography
+        variant='body2'
+        color='textSecondary'
+        component='div'
+        className={getDisplayLabels()}
+      >
+        <p className={classes.cardTitle}>
+          {item?.fields["Title (from ID copy)"]}
+        </p>
+      </Typography>
+      <Typography
+        variant='body2'
+        color='textSecondary'
+        component='p'
+        className={getDisplayLabels()}
+      >
+        <b>Published: </b>{" "}
+        {item?.fields["Date (from Article)"]}
+      </Typography>
+      <Typography
+        variant='body2'
+        color='textSecondary'
+        component='p'
+        className={getDisplayLabels()}
+      >
+        <b>Publisher: </b>{" "}
+        {item?.fields["Publisher (from ID copy)"]}
+      </Typography>
+      <Typography
+        variant='body2'
+        color='textSecondary'
+        component='p'
+        className={getDisplayLabels()}
+      >
+        <b>Country: </b>{" "}
+        {item?.fields["Country (from ID copy)"]}
+      </Typography>
+      <div className={classNames(classes.cardButtons, getDisplayLabels())}>
+        <Button
+          variant='contained'
+          disableElevation
+          className={classes.links}
+          onClick={() => handleOpen(item)}
+          target='_blank'
+        >
+          Quick Look
+        </Button>
+        <Button
+          variant='contained'
+          disableElevation
+          className={classes.links}
+          href={item.fields["URL (from ID copy)"][0]}
+          target='_blank'
+        >
+          Visit
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default GridContent;
