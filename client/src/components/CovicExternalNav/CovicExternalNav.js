1b2cb8d87253c9cc0fdbaf99313645a5a597d40d
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2021-10-15 15:10:49 -0700

    CV-33 map menu items

--- client/src/components/CovicExternalNav/CovicExternalNav.js
+++ client/src/components/CovicExternalNav/CovicExternalNav.js
@@ -3,9 +3,10 @@ import { createTheme } from "@material-ui/core/styles";
 
 import { useStyles } from "./stylesCovicExternalNav";
 
+const MENU_ITEMS = ["COVIC Is", "COVIC Contains", "logo", "COVIC Team"];
+
 const CovicExternalNav = () => {
   const theme = createTheme();
-
   const classes = useStyles(theme);
 
   const handleMenuClick = e => {
@@ -21,14 +22,19 @@ const CovicExternalNav = () => {
     }
   }
 
+  const renderNavItems = () => MENU_ITEMS.map(item => (
+      <li className={classes.covicExternalNavListItem}>
+        {item === "logo" 
+          ? <img className={classes.covicLogo} src="./assets/logo.svg" alt="" />
+          : <div className={classes.covicMenuLabel} onClick={handleMenuClick}>{item}</div>
+        }
+      </li>
+    )
+  )
+
   return (
     <div className={classes.covicExternalNavMenuHolder}>
-      <ul className={classes.covicExternalNavList}>
-        <li className={classes.covicExternalNavListItem}><div className={classes.covicMenuLabel} onClick={handleMenuClick}>COVIC Is</div></li>
-        <li className={classes.covicExternalNavListItem}><div className={classes.covicMenuLabel} onClick={handleMenuClick}>COVIC Contains</div></li>
-        <li className={classes.covicExternalNavListItem}><img className={classes.covicLogo} src="./assets/logo.svg" alt="" /></li>
-        <li className={classes.covicExternalNavListItem}><div className={classes.covicMenuLabel} onClick={handleMenuClick}>COVIC Team</div></li>
-      </ul>
+      <ul className={classes.covicExternalNavList}>{renderNavItems()}</ul>
     </div>
   )
 }
