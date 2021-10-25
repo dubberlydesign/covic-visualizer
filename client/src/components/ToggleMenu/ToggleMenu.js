b8e7c30850015adfa017e61c57000841763d237b
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2021-10-24 18:41:22 -0700

    CV-47 add labels

--- client/src/components/ToggleMenu/ToggleMenu.js
+++ client/src/components/ToggleMenu/ToggleMenu.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { createTheme } from "@material-ui/core/styles";
 import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 import { useStyles } from "./stylesToggleMenu";
@@ -27,13 +28,19 @@ const ToggleMenu = ({toggleOrder, toggleLabel}) => {
     <div>
       <FormControlLabel
         control={
-          <Switch 
-            className={classes.toggleSwitchOrder} 
-            checked={checkedOrder} 
-            onChange={handleSwitchChangeOrder} 
-            name="checkedOrder" 
-            color="default"
-          /> 
+          <Grid component="label" container alignItems="center" spacing={1}>
+            <Grid item>New</Grid>
+              <Grid item>
+                <Switch 
+                  className={classes.toggleSwitchOrder} 
+                  checked={checkedOrder} 
+                  onChange={handleSwitchChangeOrder} 
+                  name="checkedOrder" 
+                  color="default"
+                />
+              </Grid>
+            <Grid item>Old</Grid>
+          </Grid>
         }
         label="Published Date"
         className={classes.toggleSwitchOrderLabel}
@@ -41,13 +48,19 @@ const ToggleMenu = ({toggleOrder, toggleLabel}) => {
       />
       <FormControlLabel
         control={
-          <Switch 
-            className={classes.toggleSwitchOrder} 
-            checked={checkedLabel} 
-            onChange={handleSwitchChangeLabel} 
-            name="checkedLabel"
-            color="default" 
-          /> 
+          <Grid component="label" container alignItems="center" spacing={1}>
+            <Grid item>On</Grid>
+              <Grid item>
+                <Switch 
+                  className={classes.toggleSwitchOrder} 
+                  checked={checkedLabel} 
+                  onChange={handleSwitchChangeLabel} 
+                  name="checkedLabel"
+                  color="default" 
+                />
+              </Grid>
+            <Grid item>Off</Grid>
+          </Grid> 
         }
         label="Labels"
         className={classes.toggleSwitchOrderLabel}
