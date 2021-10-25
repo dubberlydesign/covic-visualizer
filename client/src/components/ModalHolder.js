536ddd5137465fbd57dfb18916fe8b6b791946c2
Wilbert Mui <wmui51@yahoo.com>
2021-10-24 18:32:52 -0700

    remove commented code

--- client/src/components/ModalHolder.js
+++ client/src/components/ModalHolder.js
@@ -35,14 +35,12 @@ const ModalHolder = ({
         aria-describedby='transition-modal-description'
         className={classes.modal}
         open={open}
-        // onBackdropClick={() => setModalState('figures')}
         onClose={(event, reason) => {
           handleClose(event);
           if (reason === "backdropClick") {
             setModalState('figures')
           }
         }}
-        // onClose={handleClose}
         closeAfterTransition
         BackdropComponent={Backdrop}
         BackdropProps={{
