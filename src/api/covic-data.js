2af57bb84f491952ac893428b2553766c1b3ad95
robbie roxas <robbieroxas@robbies-MacBook-Pro.local>
2021-10-24 18:50:47 -0700

    CV-46 remove consoles

--- src/api/covic-data.js
+++ src/api/covic-data.js
@@ -89,8 +89,6 @@ router.get("/", limiter, speedLimiter, async (req, res, next) => {
     view: req.query.inOrderDisplay === "true" ? "API [DO NOT EDIT] old-new" : "API [DO NOT EDIT]",
   };
 
-  console.log('req.query', req.query);
-
   if (
     req.query.filterType !== "" &&
     req.query.queryType !== "search" &&
